ffcaa97740e22293e966ea70f85c8e9bb3110c34
a-kenji <aks.kenji@protonmail.com>
2023-06-24 14:19:48 +0200

    feat: expand serde attributes for the TestBuffer

--- src/layout.rs
+++ src/layout.rs
@@ -458,6 +458,7 @@ impl Element {
 /// A simple rectangle used in the computation of the layout and to give widgets a hint about the
 /// area they are supposed to render to.
 #[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Rect {
     pub x: u16,
     pub y: u16,
